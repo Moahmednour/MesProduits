4dcc3e521fb4d28c8f7b496fbca3019d086bad8a
Mohamed Nour Bensalah <mednourbensalah@gmail.com>
2022-11-23 09:03:03 +0100

    [AG] Create comp addP

---
+++ src/app/add-produit/add-produit.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { Produit } from '../Model/produit.model';
+import { ProduitService } from '../produit.service';
+@Component({
+  selector: 'app-add-produit',
+  templateUrl: './add-produit.component.html',
+  styleUrls: ['./add-produit.component.css'],
+})
+export class AddProduitComponent implements OnInit {
+  newProduit = new Produit();
+  constructor(private produitService: ProduitService) {}
+
+  ngOnInit(): void {}
+
+  addProduit() {
+    console.log(this.newProduit);
+    this.produitService.ajouterProduit(this.newProduit);
+  }
+}
